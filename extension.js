17fa30fa11c2711b624e3007e7bceb2cd121aa3e
David da Silva <dasilvacontin@gmail.com>
2015-02-27 15:16:07 +0100

    First extension commit.

---
+++ extension.js
@@ -0,0 +1,22 @@
+console.log('YoAPITokenConcealer v0.1')
+
+var keyCache = {}
+
+function bindClickFunction () {
+  var rows = document.getElementsByClassName('api-token')
+  Array.prototype.forEach.call(rows, function (row) {
+    var keyNode = row.getElementsByTagName('span')[1]
+    if (!keyNode) return
+    ;var key = keyNode.innerHTML
+    if (keyCache[key]) return
+    ;keyCache[key] = true
+    keyNode.style.display = 'none'
+    var icon = row.getElementsByTagName('i')[0]
+    icon.style.cursor = 'pointer'
+    icon.onclick = function (e) {
+      keyNode.style.display = (keyNode.style.display === 'none' ? 'inline' : 'none')
+    }
+  })
+}
+
+document.addEventListener('DOMNodeInserted', bindClickFunction, false)
