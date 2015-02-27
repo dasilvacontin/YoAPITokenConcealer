fda92ff752e62628c5107e1112f5fe50d89b3eef
David da Silva <dasilvacontin@gmail.com>
2015-02-27 15:28:43 +0100

    added  linting package

--- extension.js
+++ extension.js
@@ -7,9 +7,9 @@ function bindClickFunction () {
   Array.prototype.forEach.call(rows, function (row) {
     var keyNode = row.getElementsByTagName('span')[1]
     if (!keyNode) return
-    ;var key = keyNode.innerHTML
+    var key = keyNode.innerHTML
     if (keyCache[key]) return
-    ;keyCache[key] = true
+    keyCache[key] = true
     keyNode.style.display = 'none'
     var icon = row.getElementsByTagName('i')[0]
     icon.style.cursor = 'pointer'
